517c7c048ae2a2d8e294e0bb5145c05caf9b2e30
Aartilanke <aartilanke3108@gmail.com>
2024-12-30 20:21:17 +0000

    2 instance

--- main.tf
+++ main.tf
@@ -1,17 +1,35 @@
 
 provider "aws" {
-  region = "ap-south-1"
+  region  = "ap-south-1"
   profile = "default"
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "my_instance_1" {
   ami           = "ami-03c68e52484d7488f"
   instance_type = "t2.micro"  # Choose an instance type
 
   tags = {
-    Name = "MyTerraformInstance"
+    Name = "MyTerraformInstance1"
   }
 
   key_name = "tera"  # Replace with your EC2 key pair name
-  
+}
+
+resource "aws_instance" "my_instance_2" {
+  ami           = "ami-03c68e52484d7488f"
+  instance_type = "t2.micro"  # Choose an instance type
+
+  tags = {
+    Name = "MyTerraformInstance2"
+  }
+
+  key_name = "tera"  # Replace with your EC2 key pair name
+}
+
+output "instance_1_public_ip" {
+  value = aws_instance.my_instance_1.public_ip
+}
+
+output "instance_2_public_ip" {
+  value = aws_instance.my_instance_2.public_ip
 }
