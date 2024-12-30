2cb779e182c6406830c0e968cdeeea4483e7f13f
Aartilanke <aartilanke3108@gmail.com>
2024-12-30 14:04:19 +0000

    1

---
+++ main.tf
@@ -0,0 +1,17 @@
+# main.tf
+provider "aws" {
+  region = "us-west-2" # Replace with your desired region
+  #ap-south-1
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-03c68e52484d7488f"
+  instance_type = "t2.micro"  # Choose an instance type
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+
+  key_name = "tera"  # Replace with your EC2 key pair name
+  
+}
